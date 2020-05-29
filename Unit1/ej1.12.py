b2f23bba356e4faadcd71ebb003aad7db1769d0d
Federico <65924848+ftorresi@users.noreply.github.com>
2020-05-29 15:09:03 -0300

    Update ej1.12.py

--- Unit1/ej1.12.py
+++ Unit1/ej1.12.py
@@ -1,13 +1,13 @@
 from math import pi, log
-#everything in S.I. units
+
 m = 67.0   #egg mass in g
 K = 5.4e-3  #thermal conductivity
 c = 3.7  #specific heat
 rho = 1.038 #egg density 
 
-T0=4.0 #egg initial temp.
-Tw=100.0 #boiling water temp.
-Ty=70.0 #egg target temp.
+T0=4.0 #egg initial temp. in C
+Tw=100.0 #boiling water temp. in C
+Ty=70.0 #egg target temp. in C
 
 t=m**(2./3)*c*rho**(1./3)/(K*pi**2*(4*pi/3.)**(2./3.))*log(0.76*(T0-Tw)/(Ty-Tw)) #time needed
  
